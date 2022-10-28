84b5ffa176f5eedd694a7c5bb5d535beede532ab
seattleparasail <seattleparasail@gmail.com>
2022-10-28 16:42:54 -0700

    chore: initial update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './Home';
 import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        
+        {/* <Route path="teams" element={<Teams />}>
+          <Route path=":teamId" element={<Team />} />
+          <Route path="new" element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} />
+        </Route> */}
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
