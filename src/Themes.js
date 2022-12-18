efa48ca943b2459cbde0bb504032b3f144fa2120
Havasu Parasail <seattleparasail@gmail.com>
2022-12-17 23:11:20 -0400

    style test

--- src/Themes.js
+++ src/Themes.js
@@ -2,9 +2,10 @@ export const theme = {
     colors: {
         pioneerMaroon: '#A60B37',
         emeraldCityGreen: '#238C00',
+        emeraldCityGreen_50_Opacity: '#238C0099',
         cascadeForestGreen: '#0D4700',
+        cascadeForestGreen_50_Opacity: '#0D470099',
         yellowGold: '#FFD100',
         galaxyGold: '#E8A900',
-        cascadeForestGreen_50_Opacity: '#0D470099'
     }
 }
