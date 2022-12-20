4b3545a42514bd002166205797351511eab70974
Alan Campbell <dev.projects.44@gmail.com>
2022-12-20 17:48:33 -0400

    chore

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,8 @@ import React, {useState, useEffect, useLayoutEffect} from "react";
 import SkylineSVG from './images/SeattleSkyline'
 import ForestSVG from './images/Forest'
 import SmileyChute from './images/SmileyChute.jpg'
+import SmileyChuteMobile from './images/SmileyChuteMobile.jpg'
+
 import HomeOptions from './HomeOptions'
 import { useMediaQuery, Grid } from '@mui/material'
 import styled from "@emotion/styled";
@@ -11,7 +13,7 @@ const IntroTextBox = styled.div(
     position: 'absolute',
     color: 'white',
     maxWidth: '250px',
-    top: '45vh',
+    top: '10vh',
     padding: '20px',
     fontSize: '20px',
     zIndex: 2,
@@ -32,27 +34,27 @@ const IntroTextBox = styled.div(
 
 const Home = () => {
 
-  const [pageHeight, setPageHeight] = useState(window.innerHeight)
+  // const [pageHeight, setPageHeight] = useState(window.innerHeight)
 
-  const updateSize = () => setPageHeight(window.innerHeight)
+  // const updateSize = () => setPageHeight(window.innerHeight)
 
 
-  useLayoutEffect(() => {
+  // useLayoutEffect(() => {
     
-    window.addEventListener('resize', updateSize)
-    return () => window.removeEventListener('resize', updateSize);
-  }, [])
+  //   window.addEventListener('resize', updateSize)
+  //   return () => window.removeEventListener('resize', updateSize);
+  // }, [])
 
-  const atLeast600 = useMediaQuery('(min-width:600px)');
+  const lessThan600 = useMediaQuery('(max-width:600px)');
   return (
 
     <Grid container>
 
       <Grid item xs={12}>
         <div style={{
-          background: `url(${SmileyChute}) no-repeat center center fixed`,
+          background: `url(${!lessThan600 ? SmileyChute : SmileyChuteMobile}) no-repeat center 60%  fixed`,
           backgroundSize: 'cover',
-          height: atLeast600 ? `${pageHeight + 100}px` : `${pageHeight + 25}px`
+          height: !lessThan600 ? `calc(100vh + 100px)` : `calc(100vh + 25px)`,
           }}/>
         <IntroTextBox>
           <p style={{fontWeight: 900}}>
@@ -62,8 +64,8 @@ const Home = () => {
           The Emerald City shines from a Seahawk's perspective. Explore and book your tour.
           </p>
         </IntroTextBox>
-        <SkylineSVG style={{  position: 'absolute', top: `${atLeast600 ? pageHeight - 200 : pageHeight-90}px`}} />
-        <ForestSVG style={{ height: '100px', position: 'absolute', width: '100%', top: `${atLeast600 ? pageHeight + 25 : pageHeight -35}px` }}  />
+        <SkylineSVG style={{ position: 'absolute', top: !lessThan600 ? `calc(100vh - 200px)` : `calc(100vh - 90px)`}} />
+        <ForestSVG style={{ height: '100px', position: 'absolute', width: '100%', top: !lessThan600 ? `calc(100vh + 25px)` : `calc(100vh - 35px)` }} />
       </Grid>
       
       <Grid item xs={12}>
