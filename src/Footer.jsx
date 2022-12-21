9fec31b75d2ff416f62ebdf90af3bfcda2b8e0ca
Alan Campbell <dev.projects.44@gmail.com>
2022-12-21 02:15:48 -0400

    push to save

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,8 +5,19 @@ import { theme } from './Themes'
 
 const FooterText = styled.p({
     color: theme.colors.galaxyGold,
-    marginLeft: '15px',
+    marginLeft: '3px',
     cursor: 'pointer',
+    fontSize: '11px',
+    fontWeight: 500,
+    '@media(min-width: 768px)': {
+        marginLeft: '10px',
+        fontSize: '14px',
+    },
+    '@media(min-width: 1000px)': {
+        marginLeft: '15px',
+        fontSize: '18px',
+    }
+
 })
 
 const handleLocationClick = () => window.open('https://goo.gl/maps/uecCbZ6z5SoeE5Bs7', '_blank');
@@ -16,7 +27,9 @@ const Footer = () => {
 
     return (
         <FooterContainer>
-            <Grid container>
+            <Grid container spacing={1} direction="row"
+                justify="flex-end"
+                alignItems="center">
 
                 <Grid item xs={4}>
                     <FooterText onClick={handleLocationClick}>
@@ -49,7 +62,7 @@ const FooterContainer = styled.div(
         marginTop: '80px',
         minHeight: '100px',
         width: '100%',
-        backgroundColor: theme.colors.cascadeForestGreen_50_Opacity
+        backgroundColor: theme.colors.cascadeForestGreen_75_Opacity
     }
 )
 
